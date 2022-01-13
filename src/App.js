599e9168b68d3c4cc3005b04c1303240dff81d45
Amil Rustamov <amil93.rustamov@gmail.com>
2022-01-13 09:23:16 +0400

    added project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type='text' />
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading..</div>
+      <div>Error</div>
+    </div>
+  );
+}
+
+export default App;
